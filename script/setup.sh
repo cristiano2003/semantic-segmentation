561572d97d9e2c1473e06fb470b908374205c403
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-22 15:50:42 +0700

    fix

--- script/setup.sh
+++ script/setup.sh
@@ -2,8 +2,10 @@
 pip install -e .
 
 # curl for dataset, download if not already downloaded
-
-echo "polyp not downloaded, downloading now"
-curl -L "https://drive.google.com/uc?id=1NIQU5s2maodQeJW8UEzizB24pE2p-Ww5&export=download" > "TrainDataset.zip"
-unzip -q "TrainDataset.zip" -d "TrainDataset"
+if [ -d "neopolyp" ]; then
+  echo "neopolyp already downloaded"
+else
+  echo "neopolyp not downloaded, downloading now"
+  curl -L "https://drive.google.com/uc?id=1NIQU5s2maodQeJW8UEzizB24pE2p-Ww5&export=download" > "TrainDataset.zip"
+  unzip -q "TrainDataset.zip" -d "TrainDataset"
 fi
