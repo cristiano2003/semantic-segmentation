b97286aa21135823c4410c490535b2d094a9c1f8
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-22 16:14:06 +0700

    fix

--- script/train.sh
+++ script/train.sh
@@ -5,5 +5,5 @@ python -m neopolyp.train \
         --num_workers 4 \
         --lr 0.0001 \
         --split_ratio 0.96 \
-        --data_path TrainDatset \
+        --data_path TrainDataset \
         -w -wk 53f5746150b2ce7b0552996cb6acc3beec6e487f
