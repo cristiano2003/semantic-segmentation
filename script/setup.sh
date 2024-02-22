679b42eff36beb683d209f06cadc268133dcf59c
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-22 15:58:41 +0700

    fix

--- script/setup.sh
+++ script/setup.sh
@@ -6,6 +6,6 @@ if [ -d "neopolyp" ]; then
   echo "neopolyp already downloaded"
 else
   echo "neopolyp not downloaded, downloading now"
-  curl -L "https://drive.google.com/uc?id=1NIQU5s2maodQeJW8UEzizB24pE2p-Ww5&export=download" > "TrainDataset.zip"
+  curl -L "https://drive.google.com/uc?id=1NIQU5s2maodQeJW8UEzizB24pE2p-Ww5&export=download" > "TrainDataset"
   unzip -q "TrainDataset.zip" -d "TrainDataset"
 fi
--- test.py
+++
@@ -1,5 +0,0 @@
-all_path = []
-import os
-data_path = 'TrainDataset/image'
-for file in os.listdir(data_path):
-    all_path.append(os.path.join(data_path, file))
