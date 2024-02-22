bf523d6618d5f03c623ac5b7621ea619c54ea428
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-22 16:24:44 +0700

    fix

--- script/train.sh
+++ script/train.sh
@@ -1,9 +1,9 @@
 python -m neopolyp.train \
         --model unet \
-        --batch_size 16 \
+        --batch_size 32 \
         --max_epochs 25 \
         --num_workers 4 \
         --lr 0.0001 \
-        --split_ratio 0.96 \
+        --split_ratio 0.9 \
         --data_path TrainDataset \
         -w -wk 53f5746150b2ce7b0552996cb6acc3beec6e487f
