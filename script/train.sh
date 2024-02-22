a060903680c985304a0a573489f8f9d60bdf7585
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-22 12:41:39 +0700

    fix

--- script/train.sh
+++ script/train.sh
@@ -2,7 +2,7 @@ python -m neopolyp.train \
         --model unet \
         --batch_size 64 \
         --max_epochs 50 \
-        --num_workers 2 \
+        --num_workers 4 \
         --lr 0.0001 \
         --split_ratio 0.9 \
         --data_path bkai-igh-neopolyp \
