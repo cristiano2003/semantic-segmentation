a37c8e4fa130f27ba025b1462dad2d30965836bf
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-24 15:05:13 +0700

    fix

--- script/train.sh
+++ script/train.sh
@@ -5,5 +5,5 @@ python -m neopolyp.train \
         --num_workers 4 \
         --lr 0.0001 \
         --split_ratio 0.9 \
-        --data_path /kaggle/input/pascal-voc-2012-dataset
+        --data_path pascal-voc-2012-dataset
         # -w -wk 53f5746150b2ce7b0552996cb6acc3beec6e487f
