6da77957dea66ded982075184d951ad397596404
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-24 16:43:48 +0700

    fix

--- script/setup.sh
+++ script/setup.sh
@@ -1,12 +1,12 @@
 # install package
 pip install -e .
 
-# curl for dataset, download if not already downloaded
-# if [ -d "data" ]; then
-#   echo "data already downloaded"
-# else
-#   echo "data not downloaded, downloading now"
-#   curl -L "https://www.kaggle.com/datasets/lqdisme/cityscapes/download?datasetVersionNumber=1" > "data.zip"
-#   unzip -q "data.zip" -d "data"
-# fi
+curl for dataset, download if not already downloaded
+if [ -d "data" ]; then
+  echo "data already downloaded"
+else
+  echo "data not downloaded, downloading now"
+  curl -L "https://drive.google.com/uc?id=1MPlji4PXxA-b4C_b6YRMLF5s_n_TL5fP&export=download" > "data.zip"
+  unzip -q "data.zip" -d "data"
+fi
 
