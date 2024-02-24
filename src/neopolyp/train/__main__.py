05353337924a4f6223dfb5e0be1ab1c2c87f717e
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-24 17:10:18 +0700

    fix

--- src/neopolyp/train/__main__.py
+++ src/neopolyp/train/__main__.py
@@ -66,14 +66,13 @@ def main():
         logger = None
 
     # DATALOADER
-    path = args.data_path
     all_path = []
-    image_path = '../dataset/training/image_2'
+    image_path = os.path.join(args.data_path, 'image_2')
     for file in os.listdir(image_path):
         all_path.append(os.path.join(image_path, file))
     
     all_gt_path = []
-    image_gt_path = '../dataset/training/gt_image_2'
+    image_gt_path = os.path.join(args.data_path, 'gt_image_2')
     for file in os.listdir(image_gt_path):
         all_gt_path.append(os.path.join(image_path, file))
          
