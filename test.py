ffbfc670882772d7a3534089b4294f6dd9a46b37
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-24 17:19:15 +0700

    fix

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import os
 
 all_path = []
-image_path = 'dataset/training/image_2'
+image_path = 'data/archive-2/training/image_2'
 for file in os.listdir(image_path):
     all_path.append(os.path.join(image_path, file))
     
 all_gt_path = []
-image_gt_path = 'dataset/training/gt_image_2'
+image_gt_path = 'data/archive-2/training/gt_image_2'
 for file in os.listdir(image_gt_path):
     all_gt_path.append(os.path.join(image_path, file))
         
