751022a32e3c51aa0a5c9c1f3caa5282e24e14b7
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-25 18:41:07 +0700

    fix

--- src/neopolyp/model/model.py
+++ src/neopolyp/model/model.py
@@ -22,6 +22,8 @@ class NeoPolypModel(pl.LightningModule):
 
     def _forward(self, batch, batch_idx, name="train"):
         image, mask = batch[0].float(), batch[1].long()
+        print(mask.max())
+        print(mask.min())
         logits = self(image)
         loss = self.entropy_loss(logits, mask)
         # d_score = dice_score(logits, mask)
