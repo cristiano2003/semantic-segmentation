2f7ffa3cbedd7c80339dfd567407775731bcae34
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-27 01:26:39 +0700

    fix

--- src/neopolyp/dataset/transforms.py
+++ src/neopolyp/dataset/transforms.py
@@ -1,192 +1,62 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-"""
-Transforms and data augmentation for both image + bbox.
-"""
 import random
 import numpy as np
-import PIL
-import torch
+from torchvision.transforms import functional as F
+from PIL import Image
 import torchvision.transforms as T
-import torchvision.transforms.functional as F
+import torch
+from .augment import apply_op_both,rand_augment_both
 
-from ..util.box_ops import box_xyxy_to_cxcywh
-from ..util.misc import interpolate
+class Compose(object):
+    """
+    Composes a sequence of transforms.
+    Arguments:
+        transforms: A list of transforms.
+    """
+    def __init__(self, transforms):
+        self.transforms = transforms
 
+    def __call__(self, image, label):
+        for t in self.transforms:
+            image, label = t(image, label)
+        return image, label
 
-def crop(image, target, region):
-    cropped_image = F.crop(image, *region)
-
-    target = target.copy()
-    i, j, h, w = region
-
-    # should we do something wrt the original size?
-    target["size"] = torch.tensor([h, w])
-
-    fields = ["labels", "area", "iscrowd"]
-
-    if "boxes" in target:
-        boxes = target["boxes"]
-        max_size = torch.as_tensor([w, h], dtype=torch.float32)
-        cropped_boxes = boxes - torch.as_tensor([j, i, j, i])
-        cropped_boxes = torch.min(cropped_boxes.reshape(-1, 2, 2), max_size)
-        cropped_boxes = cropped_boxes.clamp(min=0)
-        area = (cropped_boxes[:, 1, :] - cropped_boxes[:, 0, :]).prod(dim=1)
-        target["boxes"] = cropped_boxes.reshape(-1, 4)
-        target["area"] = area
-        fields.append("boxes")
-
-    if "masks" in target:
-        # FIXME should we update the area here if there are no boxes?
-        target['masks'] = target['masks'][:, i:i + h, j:j + w]
-        fields.append("masks")
-
-    # remove elements for which the boxes or masks that have zero area
-    if "boxes" in target or "masks" in target:
-        # favor boxes selection when defining which elements to keep
-        # this is compatible with previous implementation
-        if "boxes" in target:
-            cropped_boxes = target['boxes'].reshape(-1, 2, 2)
-            keep = torch.all(cropped_boxes[:, 1, :] > cropped_boxes[:, 0, :], dim=1)
-        else:
-            keep = target['masks'].flatten(1).any(1)
-
-        for field in fields:
-            target[field] = target[field][keep]
-
-    return cropped_image, target
+    def __repr__(self):
+        format_string = self.__class__.__name__ + "("
+        for t in self.transforms:
+            format_string += "\n"
+            format_string += "    {0}".format(t)
+        format_string += "\n)"
+        return format_string
 
 
-def hflip(image, target):
-    flipped_image = F.hflip(image)
+class ToTensor(object):
+    def __call__(self, image, target):
+        image = F.to_tensor(image)
+        target = torch.as_tensor(np.array(target), dtype=torch.int64)
+        return image, target
 
-    w, h = image.size
-
-    target = target.copy()
-    if "boxes" in target:
-        boxes = target["boxes"]
-        boxes = boxes[:, [2, 1, 0, 3]] * torch.as_tensor([-1, 1, -1, 1]) + torch.as_tensor([w, 0, w, 0])
-        target["boxes"] = boxes
-
-    if "masks" in target:
-        target['masks'] = target['masks'].flip(-1)
-
-    return flipped_image, target
-
-
-def resize(image, target, size, max_size=None):
-    # size can be min_size (scalar) or (w, h) tuple
-
-    def get_size_with_aspect_ratio(image_size, size, max_size=None):
-        w, h = image_size
-        if max_size is not None:
-            min_original_size = float(min((w, h)))
-            max_original_size = float(max((w, h)))
-            if max_original_size / min_original_size * size > max_size:
-                size = int(round(max_size * min_original_size / max_original_size))
-
-        if (w <= h and w == size) or (h <= w and h == size):
-            return (h, w)
-
-        if w < h:
-            ow = size
-            oh = int(size * h / w)
-        else:
-            oh = size
-            ow = int(size * w / h)
-
-        return (oh, ow)
-
-    def get_size(image_size, size, max_size=None):
-        if isinstance(size, (list, tuple)):
-            return size[::-1]
-        else:
-            return get_size_with_aspect_ratio(image_size, size, max_size)
-
-    size = get_size(image.size, size, max_size)
-    rescaled_image = F.resize(image, size)
-
-    if target is None:
-        return rescaled_image, None
-
-    ratios = tuple(float(s) / float(s_orig) for s, s_orig in zip(rescaled_image.size, image.size))
-    ratio_width, ratio_height = ratios
-
-    target = target.copy()
-    if "boxes" in target:
-        boxes = target["boxes"]
-        scaled_boxes = boxes * torch.as_tensor([ratio_width, ratio_height, ratio_width, ratio_height])
-        target["boxes"] = scaled_boxes
-
-    if "area" in target:
-        area = target["area"]
-        scaled_area = area * (ratio_width * ratio_height)
-        target["area"] = scaled_area
-
-    h, w = size
-    target["size"] = torch.tensor([h, w])
-
-    if "masks" in target:
-        target['masks'] = interpolate(
-            target['masks'][:, None].float(), size, mode="nearest")[:, 0] > 0.5
-
-    return rescaled_image, target
+class RandAugment:
+    def __init__(self,N,M,prob=1.0,fill=(128,128,128),ignore_value=255):
+        self.N=N
+        self.M=M
+        self.prob=prob
+        self.fill=fill
+        self.ignore_value=ignore_value
+    def __call__(self, image, target):
+        return rand_augment_both(image,target,n_ops=self.N,magnitude=self.M,prob=self.prob,fill=self.fill,ignore_value=self.ignore_value)
 
 
-def pad(image, target, padding):
-    # assumes that we only pad on the bottom right corners
-    padded_image = F.pad(image, (0, 0, padding[0], padding[1]))
-    if target is None:
-        return padded_image, None
-    target = target.copy()
-    # should we do something wrt the original size?
-    target["size"] = torch.tensor(padded_image.size[::-1])
-    if "masks" in target:
-        target['masks'] = torch.nn.functional.pad(target['masks'], (0, padding[0], 0, padding[1]))
-    return padded_image, target
+class Normalize(object):
+    """
+    Normalizes image by mean and std.
+    """
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
 
-
-class RandomCrop(object):
-    def __init__(self, size):
-        self.size = size
-
-    def __call__(self, img, target):
-        region = T.RandomCrop.get_params(img, self.size)
-        return crop(img, target, region)
-
-
-class RandomSizeCrop(object):
-    def __init__(self, min_size: int, max_size: int):
-        self.min_size = min_size
-        self.max_size = max_size
-
-    def __call__(self, img: PIL.Image.Image, target: dict):
-        w = random.randint(self.min_size, min(img.width, self.max_size))
-        h = random.randint(self.min_size, min(img.height, self.max_size))
-        region = T.RandomCrop.get_params(img, [h, w])
-        return crop(img, target, region)
-
-
-class CenterCrop(object):
-    def __init__(self, size):
-        self.size = size
-
-    def __call__(self, img, target):
-        image_width, image_height = img.size
-        crop_height, crop_width = self.size
-        crop_top = int(round((image_height - crop_height) / 2.))
-        crop_left = int(round((image_width - crop_width) / 2.))
-        return crop(img, target, (crop_top, crop_left, crop_height, crop_width))
-
-
-class RandomHorizontalFlip(object):
-    def __init__(self, p=0.5):
-        self.p = p
-
-    def __call__(self, img, target):
-        if random.random() < self.p:
-            return hflip(img, target)
-        return img, target
-
+    def __call__(self, image, label):
+        image = F.normalize(image, mean=self.mean, std=self.std)
+        return image, label
 
 class RandomResize(object):
     def __init__(self, min_size, max_size=None):
@@ -196,77 +66,161 @@ class RandomResize(object):
         self.max_size = max_size
 
     def __call__(self, image, target):
-        size = random.randint(self.min_size, self.max_size)
+        size = self.max_size
         image = F.resize(image, size)
         target = F.resize(target, size, interpolation=F.InterpolationMode.NEAREST)
         return image, target
 
+class ColorJitter:
+    def __init__(self,brightness=0.2, contrast=0.2, saturation=(0.5,4), hue=0.2):
+        self.jitter=T.ColorJitter(brightness=brightness, contrast=contrast, saturation=saturation, hue=hue)
+    def __call__(self, image, target):
+        image=self.jitter(image)
+        return image,target
 
-class RandomPad(object):
-    def __init__(self, max_pad):
-        self.max_pad = max_pad
+class AddNoise:#additive gaussian noise
+    def __init__(self,factor):
+        self.factor=factor
+    def __call__(self, image, target):
+        factor = random.uniform(0, self.factor)
+        image = np.array(image)
+        assert(image.dtype==np.uint8)
+        gauss = (np.array(torch.randn(*image.shape)) * factor).astype("uint8")
+        noisy = (image + gauss).clip(0, 255)
+        image = Image.fromarray(noisy)
+        return image, target
 
-    def __call__(self, img, target):
-        pad_x = random.randint(0, self.max_pad)
-        pad_y = random.randint(0, self.max_pad)
-        return pad(img, target, (pad_x, pad_y))
+class RandomRotation:
+    def __init__(self,degrees,mean,ignore_value=255):
+        self.degrees=degrees
+        self.mean=mean
+        self.ignore_value=ignore_value
+    def __call__(self, image, target):
+        expand=True
+        if random.random()<0.5:
+            angle = random.uniform(*self.degrees)
+            image=F.rotate(image, angle,fill=self.mean,expand=expand)
+            target=F.rotate(target,angle,fill=self.ignore_value,expand=expand)
+        return image,target
 
 
-class RandomSelect(object):
+class RandomScale(object):
     """
-    Randomly selects between transforms1 and transforms2,
-    with probability p for transforms1 and (1 - p) for transforms2
+    Applies random scale augmentation.
+    Arguments:
+        min_scale: Minimum scale value.
+        max_scale: Maximum scale value.
+        scale_step_size: The step size from minimum to maximum value.
     """
-    def __init__(self, transforms1, transforms2, p=0.5):
-        self.transforms1 = transforms1
-        self.transforms2 = transforms2
-        self.p = p
+    def __init__(self, min_scale, max_scale, scale_step_size):
+        self.min_scale = min_scale
+        self.max_scale = max_scale
+        self.scale_step_size = scale_step_size
 
-    def __call__(self, img, target):
-        if random.random() < self.p:
-            return self.transforms1(img, target)
-        return self.transforms2(img, target)
+    @staticmethod
+    def get_random_scale(min_scale_factor, max_scale_factor, step_size):
+        """Gets a random scale value.
+        Args:
+            min_scale_factor: Minimum scale value.
+            max_scale_factor: Maximum scale value.
+            step_size: The step size from minimum to maximum value.
+        Returns:
+            A random scale value selected between minimum and maximum value.
+        Raises:
+            ValueError: min_scale_factor has unexpected value.
+        """
+        if min_scale_factor < 0 or min_scale_factor > max_scale_factor:
+            raise ValueError('Unexpected value of min_scale_factor.')
+
+        if min_scale_factor == max_scale_factor:
+            return min_scale_factor
+
+        # When step_size = 0, we sample the value uniformly from [min, max).
+        if step_size == 0:
+            return random.uniform(min_scale_factor, max_scale_factor)
+
+        # When step_size != 0, we randomly select one discrete value from [min, max].
+        num_steps = int((max_scale_factor - min_scale_factor) / step_size + 1)
+        scale_factors = np.linspace(min_scale_factor, max_scale_factor, num_steps)
+        np.random.shuffle(scale_factors)
+        return scale_factors[0]
+
+    def __call__(self, image, label):
+        scale = self.get_random_scale(self.min_scale, self.max_scale, self.scale_step_size)
+        img_w, img_h = image.size
+        img_w,img_h=int(img_w*scale),int(img_h*scale)
+        image=F.resize(image,[img_h,img_w])
+        label=F.resize(label,[img_h,img_w],interpolation=F.InterpolationMode.NEAREST)
+        return image,label
 
 
-class ToTensor(object):
+class RandomCrop(object):
+    def __init__(self, crop_h, crop_w, pad_value, ignore_label, random_pad):
+        self.crop_h = crop_h
+        self.crop_w = crop_w
+        self.pad_value = pad_value
+        self.ignore_label = ignore_label
+        self.random_pad = random_pad
+
+    def __call__(self, image, label):
+        img_w,img_h=image.size
+        pad_h = max(self.crop_h - img_h, 0)
+        pad_w = max(self.crop_w - img_w, 0)
+        if pad_h > 0 or pad_w > 0:
+            if self.random_pad:
+                pad_top = random.randint(0, pad_h)
+                pad_bottom = pad_h - pad_top
+                pad_left = random.randint(0, pad_w)
+                pad_right = pad_w - pad_left
+            else:
+                pad_top, pad_bottom, pad_left, pad_right = 0, pad_h, 0, pad_w
+            image = F.pad(image, (pad_left, pad_top, pad_right, pad_bottom), fill=self.pad_value)
+            label= F.pad(label, (pad_left, pad_top, pad_right, pad_bottom), fill=self.ignore_label)
+
+        crop_params = T.RandomCrop.get_params(image, (self.crop_h, self.crop_w))
+        image = F.crop(image, *crop_params)
+        label = F.crop(label, *crop_params)
+        return image,label
+
+
+class RandomHorizontalFlip(object):
+    def __init__(self, flip_prob):
+        self.flip_prob = flip_prob
+
     def __call__(self, image, target):
-        image = F.to_tensor(image)
-        target = torch.as_tensor(np.array(target), dtype=torch.int64)
+        if random.random() < self.flip_prob:
+            image = F.hflip(image)
+            target = F.hflip(target)
         return image, target
 
+def f():
+    image=np.zeros((50,50),dtype=np.uint8)
+    assert(image.dtype==np.uint8)
+    gauss=(np.array(torch.randn(50,50)*10)).astype("uint8")
+    noisy = (image + gauss).clip(0, 255)
+    # print(noisy.dtype)
+    # factor = random.uniform(0, 10)
+    # image = Image.open("cityscapes_dataset/leftImg8bit/train/aachen/aachen_000000_000019_leftImg8bit.png")
+    # image=np.array(image)
+    # gauss = np.array(torch.randn(*image.shape)) * factor
+    # noisy = (image + gauss).clip(0, 255).astype("uint8")
 
-class RandomErasing(object):
-
-    def __init__(self, *args, **kwargs):
-        self.eraser = T.RandomErasing(*args, **kwargs)
-
-    def __call__(self, img, target):
-        return self.eraser(img), target
-
-
-class Normalize(object):
-    def __init__(self, mean, std):
-        self.mean = mean
-        self.std = std
-
-    def __call__(self, image, target):
-        image = F.normalize(image, mean=self.mean, std=self.std)
-        return image, target
+def g():
+    image=np.zeros((50,50))
+    gauss=np.array(torch.randn(50,50)*10)
+    noisy = (image + gauss).clip(0, 255).astype("uint8")
+    # print(noisy.dtype)
+    # factor = random.uniform(0, 10)
+    # image = Image.open("cityscapes_dataset/leftImg8bit/train/aachen/aachen_000000_000019_leftImg8bit.png")
+    # image=np.array(image)
+    # gauss = np.array(torch.randn(*image.shape)) * factor
+    # print(gauss.dtype)
+    # noisy = (image + gauss).clip(0, 255).astype("uint8")
 
 
-class Compose(object):
-    def __init__(self, transforms):
-        self.transforms = transforms
-
-    def __call__(self, image, target):
-        for t in self.transforms:
-            image, target = t(image, target)
-        return image, target
-
-    def __repr__(self):
-        format_string = self.__class__.__name__ + "("
-        for t in self.transforms:
-            format_string += "\n"
-            format_string += "    {0}".format(t)
-        format_string += "\n)"
-        return format_string
+if __name__=='__main__':
+    f()
+    g()
+    #import timeit
+    #print(timeit.timeit('f()', globals=globals(), number=100))
+    #print(timeit.timeit('g()', globals=globals(), number=100))
