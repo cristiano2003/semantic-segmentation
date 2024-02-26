7f9347a83514b243c1e1bb9e2b08161429675338
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-26 13:30:23 +0700

    fix

--- script/setup.sh
+++ script/setup.sh
@@ -1,12 +1,12 @@
 # install package
 pip install -e .
 
-curl for dataset, download if not already downloaded
-if [ -d "data" ]; then
-  echo "data already downloaded"
-else
-  echo "data not downloaded, downloading now"
-  curl -L 'https://drive.usercontent.google.com/download?id=1x8o4juYBgwxXbRt_EuaHCxvCeOLzUcws&export=download&authuser=0&confirm=t&uuid=cf4523b7-69d7-43bb-8be7-8753062fe609&at=APZUnTVJuR1zwRnGwafvXwvWCJsv%3A1708927447409' > "data.zip"
-  unzip -q "data.zip" -d ""
-fi
+# curl for dataset, download if not already downloaded
+# if [ -d "data" ]; then
+#   echo "data already downloaded"
+# else
+#   echo "data not downloaded, downloading now"
+#   curl -L 'https://drive.usercontent.google.com/download?id=1x8o4juYBgwxXbRt_EuaHCxvCeOLzUcws&export=download&authuser=0&confirm=t&uuid=cf4523b7-69d7-43bb-8be7-8753062fe609&at=APZUnTVJuR1zwRnGwafvXwvWCJsv%3A1708927447409' > "data.zip"
+#   unzip -q "data.zip" -d ""
+# fi
 
