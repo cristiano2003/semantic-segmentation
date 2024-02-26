c31af604eb8a04079fa63ec15709b209660e4e45
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-26 13:05:18 +0700

    fix

--- script/setup.sh
+++ script/setup.sh
@@ -6,7 +6,7 @@ if [ -d "data" ]; then
   echo "data already downloaded"
 else
   echo "data not downloaded, downloading now"
-  curl -L 'https://drive.usercontent.google.com/download?id=1x8o4juYBgwxXbRt_EuaHCxvCeOLzUcws&export=download&authuser=0&confirm=t&uuid=949819b8-9325-4611-8a81-a10bfdca79dd&at=APZUnTUw2JZjwKAWr-d-VYxfE8yU%3A1708921918366' > "data.zip"
+  curl -L 'https://drive.usercontent.google.com/download?id=1x8o4juYBgwxXbRt_EuaHCxvCeOLzUcws&export=download&authuser=0&confirm=t&uuid=cf4523b7-69d7-43bb-8be7-8753062fe609&at=APZUnTVJuR1zwRnGwafvXwvWCJsv%3A1708927447409' > "data.zip"
   unzip -q "data.zip" -d ""
 fi
 
