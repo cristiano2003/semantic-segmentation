6ef3105b44106fc306a90677b7468af65a00d4fb
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-26 23:18:30 +0700

    fix

--- script/train.sh
+++ script/train.sh
@@ -5,6 +5,6 @@ python -m neopolyp.train \
         --num_workers 4 \
         --lr 0.0001 \
         --split_ratio 0.9 \
-        -- masks 
+        --masks 
         --coco_path /kaggle/input/coco-val/data 
         # -w -wk 53f5746150b2ce7b0552996cb6acc3beec6e487f
