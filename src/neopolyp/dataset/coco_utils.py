4ecc41fe14bdf94d01d408392c62f32b7f213409
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-26 22:37:55 +0700

    fix

--- src/neopolyp/dataset/coco_utils.py
+++ src/neopolyp/dataset/coco_utils.py
@@ -11,7 +11,7 @@ import torch.utils.data
 import torchvision
 from pycocotools import mask as coco_mask
 
-import dataset.transforms as T
+from .transforms import *
 
 
 class CocoDetection(torchvision.datasets.CocoDetection):
@@ -114,30 +114,30 @@ class ConvertCocoPolysToMask(object):
 
 def make_coco_transforms(image_set):
 
-    normalize = T.Compose([
-        T.ToTensor(),
-        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    normalize = Compose([
+        ToTensor(),
+        Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
     scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
 
     if image_set == 'train':
-        return T.Compose([
-            T.RandomHorizontalFlip(),
-            T.RandomSelect(
-                T.RandomResize(scales, max_size=1333),
-                T.Compose([
-                    T.RandomResize([400, 500, 600]),
-                    T.RandomSizeCrop(384, 600),
-                    T.RandomResize(scales, max_size=1333),
+        return Compose([
+            RandomHorizontalFlip(),
+            RandomSelect(
+                RandomResize(scales, max_size=1333),
+                Compose([
+                    RandomResize([400, 500, 600]),
+                    RandomSizeCrop(384, 600),
+                    RandomResize(scales, max_size=1333),
                 ])
             ),
             normalize,
         ])
         
     if image_set == 'val':
-        return T.Compose([
-            T.RandomResize([800], max_size=1333),
+        return Compose([
+            RandomResize([800], max_size=1333),
             normalize,
         ])
 
