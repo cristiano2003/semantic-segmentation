925bc30fc362c4a9fed19a7cc4c507867d0cded3
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-27 12:29:09 +0700

    fix

--- src/neopolyp/model/model.py
+++ src/neopolyp/model/model.py
@@ -21,7 +21,7 @@ class NeoPolypModel(pl.LightningModule):
         return self.model(x)
 
     def _forward(self, batch, batch_idx, name="train"):
-        image, mask = batch[0], batch[1]
+        image, mask = batch[0].float(), batch[1].long()
         logits = self(image)
         
         loss = self.entropy_loss(logits, mask)
