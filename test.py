2e2fd6fb78d1547541100a498926bdf353dc9eaf
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-28 19:53:39 +0700

    fix

--- test.py
+++ test.py
@@ -6,17 +6,17 @@ s = set()
 val =  infer_build("val")
 
     
-sample_1, sample_2  = val[0], val[1]
+sample_1, sample_2  = val[6], val[7]
 image1, mask1, image2, mask2 = torch.permute(sample_1[0], (1, 2, 0)), sample_1[1], torch.permute(sample_2[0], (1, 2, 0)), sample_2[1]
 
 
 plt.subplot(2, 2, 1)
 plt.imshow(np.array(image1))  # You can choose a different colormap
 plt.subplot(2, 2, 2)
-plt.imshow(np.array(mask1), cmap='gray')  # You can choose a different colormap
+plt.imshow(np.array(mask1))  # You can choose a different colormap
 plt.subplot(2, 2, 3)
 plt.imshow(np.array(image2))  # You can choose a different colormap
 plt.subplot(2, 2, 4)
-plt.imshow(np.array(mask2), cmap = 'gray') 
+plt.imshow(np.array(mask2)) 
 
 plt.show()
