064581cb38017ba73c730fa50508a6f80c43fde4
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-02-29 16:49:49 +0700

    fix

--- script/setup.sh
+++ script/setup.sh
@@ -7,6 +7,6 @@ if [ -d "data" ]; then
 else
   echo "data not downloaded, downloading now"
   curl -L 'https://drive.usercontent.google.com/download?id=1cBQy7BgvdhZcSVbRBwXS8tfVqj7NIiOs&export=download&confirm=t&uuid=556de15a-cbc0-40f9-ac70-ee1a5ffe58e6' > "model.ckpt.zip"
-  unzip -q "model.ckpt.zip" -d "checkpoints/model"
+  unzip -q "model.ckpt.zip" -d "checkpoints/model/"
 fi
 
