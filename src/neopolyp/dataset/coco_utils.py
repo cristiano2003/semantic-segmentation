6b61a44e678d57954f6fabcac154627536d89b79
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-03-01 13:01:24 +0700

    fix

--- src/neopolyp/dataset/coco_utils.py
+++ src/neopolyp/dataset/coco_utils.py
@@ -107,7 +107,7 @@ def build(args, mode='train'):
         "val": ("val2017", os.path.join("annotations", "instances_val2017.json")),
     }
      
-    CAT_LIST = [0, 3, 4]
+    CAT_LIST = [0, 1, 3, 4]
 
     transforms = Compose([
         FilterAndRemapCocoCategories(CAT_LIST, remap=True),
