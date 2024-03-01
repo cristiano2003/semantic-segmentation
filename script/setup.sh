50371ce07c1c81d6d0ab4bb887157211881b7b3b
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-03-01 12:54:35 +0700

    fix

--- script/setup.sh
+++ script/setup.sh
@@ -1,12 +1,12 @@
 # install package
 pip install -e .
 
-curl for dataset, download if not already downloaded
-if [ -d "data" ]; then
-  echo "data already downloaded"
-else
-  echo "data not downloaded, downloading now"
-  curl -L 'https://drive.usercontent.google.com/download?id=1cBQy7BgvdhZcSVbRBwXS8tfVqj7NIiOs&export=download&confirm=t&uuid=556de15a-cbc0-40f9-ac70-ee1a5ffe58e6' > "model.ckpt.zip"
-  unzip -q "model.ckpt.zip" -d "checkpoints/model/"
-fi
+# curl for dataset, download if not already downloaded
+# if [ -d "data" ]; then
+#   echo "data already downloaded"
+# else
+#   echo "data not downloaded, downloading now"
+#   curl -L 'https://drive.usercontent.google.com/download?id=1cBQy7BgvdhZcSVbRBwXS8tfVqj7NIiOs&export=download&confirm=t&uuid=556de15a-cbc0-40f9-ac70-ee1a5ffe58e6' > "model.ckpt.zip"
+#   unzip -q "model.ckpt.zip" -d "checkpoints/model/"
+# fi
 
