8b202b4c01c63af16a013aaa7115497076bb6e24
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-03-05 10:09:44 +0700

    fix

--- script/train.sh
+++ script/train.sh
@@ -1,7 +1,7 @@
 python -m segmentation.train \
-        --model unet \
+        --model deeplabv3plus \
         --batch_size 16 \
-        --max_epochs 25 \
+        --max_epochs 50 \
         --num_workers 4 \
         --lr 0.0001 \
         --data_path /kaggle/input/coco-2017-dataset/coco2017 \
