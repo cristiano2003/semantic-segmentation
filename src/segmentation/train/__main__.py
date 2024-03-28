2c7add9eb15c82d8b7efec755622a7ef6c8427d0
cristiano2003 <nguyentrungtruc2003@gmail.com>
2024-03-28 10:13:25 +0700

    fix

--- src/segmentation/train/__main__.py
+++ src/segmentation/train/__main__.py
@@ -71,7 +71,7 @@ def train(args, model = 'resunet'):
     sampler_val = torch.utils.data.SequentialSampler(val_dataset)
     
     batch_sampler_train = torch.utils.data.BatchSampler(
-        sampler_train, args.batch_size, drop_last=True)
+        sampler_train, args.batch_size, drop_last=False)
     
     train_loader = DataLoader(train_dataset, batch_sampler=batch_sampler_train,
                                    collate_fn=collate_fn, num_workers=args.num_workers, shuffle=True)
